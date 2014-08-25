678a520413bc4e6a9825f2b4d75e464f8fc3035f
Eero af Heurlin <rambo@iki.fi>
2014-08-25 20:51:53 +0300

    make sure the driver is reaped on exit if I forget

--- stress_test.py
+++ stress_test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import *
+import atexit
 
 REMOTE='http://10.211.55.14:4444/wd/hub'
 
@@ -11,3 +11,4 @@ ffdriver = webdriver.Remote(
                 desired_capabilities=DesiredCapabilities.FIREFOX,
                 command_executor=REMOTE
 )
+atexit.register(ffdriver.quit)
