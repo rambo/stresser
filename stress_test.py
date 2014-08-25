1a0f51786d13cd2c02c309613777cef53833f0b6
Eero af Heurlin <rambo@iki.fi>
2014-08-25 22:34:16 +0300

    try to handle errors more gracefully but chrome seems to hate me

--- stress_test.py
+++ stress_test.py
@@ -18,48 +18,53 @@ def getn(url, n):
     p = multiprocessing.current_process()
     print 'Starting:', p.name, p.pid
     sys.stdout.flush()
-    driver = webdriver.Remote(
-                    desired_capabilities=DesiredCapabilities.FIREFOX,
-                    command_executor=REMOTE
-    )
-    # This doesn't seem to work with multiprocessing...
-    atexit.register(driver.quit)
-    driver.implicitly_wait(30)
-    driver.maximize_window()
-    print 'Ready:', p.name, p.pid
-    sys.stdout.flush()
-    for i in range(n):
-        b = time.time()
-        driver.get(url)
-        took = time.time() - b
-        print "pid %d, time %f: got %s in %f seconds" % (p.pid, time.time(), url, took)
-        sys.stdout.flush()
-        b = time.time()
-        fname = "%d_%s.png" % (p.pid,datetime.datetime.now().isoformat())
-        took = time.time() - b
-        driver.get_screenshot_as_file(fname)
-        print "pid %d, time %f: dumped %s in %f seconds" % (p.pid, time.time(), fname, took)
-        sys.stdout.flush()
-
-    # Stupider wait (it sorta works (at least on FF, but then I get errors from WebDriverWait...)
-    #time.sleep(15)
-    #fname = "%d_%s.png" % (p.pid,datetime.datetime.now().isoformat())
-    #driver.get_screenshot_as_file(fname)
-
-    # Wait for slide change and grab second screenshot
     try:
-        WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.carousel-box .box-content:nth-child(2)')))
-        fname = "%d_%s.png" % (p.pid,datetime.datetime.now().isoformat())
-        driver.get_screenshot_as_file(fname)
-    except seleniumexceptions.TimeoutException,e:
-        print "pid %d, time %f: TimeOut when waiting for element!" % (p.pid, time.time())
+        driver = webdriver.Remote(
+                        desired_capabilities=DesiredCapabilities.CHROME,
+                        command_executor=REMOTE
+        )
+        # This doesn't seem to work with multiprocessing...
+        atexit.register(driver.quit)
+        driver.implicitly_wait(30)
+        driver.maximize_window()
+        print 'Ready:', p.name, p.pid
         sys.stdout.flush()
-        fname = "%d_%s_notfound.png" % (p.pid,datetime.datetime.now().isoformat())
-        driver.get_screenshot_as_file(fname)
+        for i in range(n):
+            b = time.time()
+            driver.get(url)
+            took = time.time() - b
+            print "pid %d, time %f: got %s in %f seconds" % (p.pid, time.time(), url, took)
+            sys.stdout.flush()
+            b = time.time()
+            fname = "%d_%s.png" % (p.pid,datetime.datetime.now().isoformat())
+            took = time.time() - b
+            driver.get_screenshot_as_file(fname)
+            print "pid %d, time %f: dumped %s in %f seconds" % (p.pid, time.time(), fname, took)
+            sys.stdout.flush()
+    
+        # Stupider wait (it sorta works (at least on FF, but then I get errors from WebDriverWait...)
+        #time.sleep(15)
+        #fname = "%d_%s.png" % (p.pid,datetime.datetime.now().isoformat())
+        #driver.get_screenshot_as_file(fname)
+    
+        # Wait for slide change and grab second screenshot
+        try:
+            WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.carousel-box .box-content:nth-child(2)')))
+            fname = "%d_%s.png" % (p.pid,datetime.datetime.now().isoformat())
+            driver.get_screenshot_as_file(fname)
+        except seleniumexceptions.TimeoutException,e:
+            print "pid %d, time %f: TimeOut when waiting for element!" % (p.pid, time.time())
+            sys.stdout.flush()
+            fname = "%d_%s_notfound.png" % (p.pid,datetime.datetime.now().isoformat())
+            driver.get_screenshot_as_file(fname)
 
-    print 'Done:', p.name, p.pid
-    sys.stdout.flush()
-    driver.quit()
+    except seleniumexceptions.WebDriverException, e:
+        print "pid %d, time %f: %s" % e
+        sys.stdout.flush()
+    finally:
+        print 'Done:', p.name, p.pid
+        sys.stdout.flush()
+        driver.quit()
 
 
 if __name__ == '__main__':
