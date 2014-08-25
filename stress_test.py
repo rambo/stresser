a3d8ea9d2b5cb8d3f0a8bc334bb3b62d20bdbf55
Eero af Heurlin <rambo@iki.fi>
2014-08-25 21:29:09 +0300

    try alternative approach for atexit

--- stress_test.py
+++ stress_test.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.common.exceptions import *
-import atexit
 import time, datetime
 import multiprocessing
 import sys,os
@@ -12,6 +10,7 @@ REMOTE='http://10.211.55.14:4444/wd/hub'
 
 
 def getn(url, n):
+    import atexit
     p = multiprocessing.current_process()
     print 'Starting:', p.name, p.pid
     sys.stdout.flush()
@@ -34,7 +33,7 @@ def getn(url, n):
         driver.get_screenshot_as_file(fname)
         print "pid %d, time %f: dumped %s in %f seconds" % (p.pid, time.time(), fname, took)
 
-    time.sleep(10)
+    time.sleep(20)
     fname = "%d_%s.png" % (p.pid,datetime.datetime.now().isoformat())
     driver.get_screenshot_as_file(fname)
     print 'Done:', p.name, p.pid
@@ -45,7 +44,7 @@ def getn(url, n):
 if __name__ == '__main__':
     jobs = []
     for x in range(5):
-        j = multiprocessing.Process(target=getn, args=('http://www.aalto.fi/fi/', 10))
+        j = multiprocessing.Process(target=getn, args=('http://www.aalto.fi/fi/', 3))
         jobs.append(j)
         j.start()
     
