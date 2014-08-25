a50e5bdb097de5307bb47fef251136df55f552cd
Eero af Heurlin <rambo@iki.fi>
2014-08-25 20:49:31 +0300

    make sure I can connect to the Selenium grid on my other local VM

---
+++ stress_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import *
+
+REMOTE='http://10.211.55.14:4444/wd/hub'
+
+ffdriver = webdriver.Remote(
+                desired_capabilities=DesiredCapabilities.FIREFOX,
+                command_executor=REMOTE
+)
