e804e95e12868c1f7a7464e9a8eb1485d64b9cf3
Eero af Heurlin <rambo@iki.fi>
2014-08-25 21:22:18 +0300

    test how many concurrent browsers doing stuff we can actually run...

--- stress_test.py
+++ stress_test.py
@@ -1,14 +1,53 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import *
 import atexit
+import time, datetime
+import multiprocessing
+import sys,os
 
 REMOTE='http://10.211.55.14:4444/wd/hub'
 
-ffdriver = webdriver.Remote(
-                desired_capabilities=DesiredCapabilities.FIREFOX,
-                command_executor=REMOTE
-)
-atexit.register(ffdriver.quit)
+
+def getn(url, n):
+    p = multiprocessing.current_process()
+    print 'Starting:', p.name, p.pid
+    sys.stdout.flush()
+    driver = webdriver.Remote(
+                    desired_capabilities=DesiredCapabilities.FIREFOX,
+                    command_executor=REMOTE
+    )
+    # This doesn't seem to work with multiprocessing...
+    atexit.register(driver.quit)
+    print 'Ready:', p.name, p.pid
+    sys.stdout.flush()
+    for i in range(n):
+        b = time.time()
+        driver.get(url)
+        took = time.time() - b
+        print "pid %d, time %f: got %s in %f seconds" % (p.pid, time.time(), url, took)
+        b = time.time()
+        fname = "%d_%s.png" % (p.pid,datetime.datetime.now().isoformat())
+        took = time.time() - b
+        driver.get_screenshot_as_file(fname)
+        print "pid %d, time %f: dumped %s in %f seconds" % (p.pid, time.time(), fname, took)
+
+    time.sleep(10)
+    fname = "%d_%s.png" % (p.pid,datetime.datetime.now().isoformat())
+    driver.get_screenshot_as_file(fname)
+    print 'Done:', p.name, p.pid
+    sys.stdout.flush()
+    driver.quit()
+
+
+if __name__ == '__main__':
+    jobs = []
+    for x in range(5):
+        j = multiprocessing.Process(target=getn, args=('http://www.aalto.fi/fi/', 10))
+        jobs.append(j)
+        j.start()
+    
+    for job in jobs:
+        job.join()
