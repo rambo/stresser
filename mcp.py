e71fe7d6f57c82186c4ec1fc8e4688dbe559815c
Eero af heurlin <eero.afheurlin@iki.fi>
2014-09-04 12:11:50 +0300

    a bit of code

--- mcp.py
+++ mcp.py
@@ -1,1 +1,32 @@
+#!/usr/bin/env python
 """The Master Control Program"""
+import zmq
+from zmq.eventloop import ioloop
+import zmqdecorators
+
+SERVICE_NAME='rambo.stresser.mcp'
+
+class mcp(object):
+    def __init__(self, pub_wrapper):
+        self.zmq_pub_wraper = pub_wrapper
+        pass
+
+    @zmqdecorators.signal(SERVICE_NAME)
+    def testsignal(self):
+        pass
+
+    @zmqdecorators.method(SERVICE_NAME)
+    def emit_testsignal(self, resp, *args):
+        resp.send("ok") # Not required
+        self.testsignal()
+
+    def run(self):
+        ioloop.IOLoop.instance().start()
+
+
+
+if __name__ == "__main__":
+    # We will need a slightly lower level access
+    pub_wrapper = zmqdecorators.server_tracker.get_by_name_or_create(SERVICE_NAME, zmq.PUB)
+    instance = mcp(pub_wrapper)
+    instance.run()
--- worker.py
+++ worker.py
@@ -1,2 +1,21 @@
 """Worker"""
+import zmq
+from zmq.eventloop import ioloop
+import zmqdecorators
+
+SERVICE_NAME='rambo.stresser.mcp'
+
+class worker(object):
+    def __init__(self, client_wrapper):
+        self.zmq_wraper = client_wrapper
+        pass
+
+    def run(self):
+        ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    # We will need a slightly lower level access
+    client_wrapper = zmqdecorators.zmq_bonjour_connect_wrapper(zmq.DEALER, 'rambo.stresser.mcp')
+    instance = worker(client_wrapper)
+    instance.run()
 
