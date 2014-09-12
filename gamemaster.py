88b1b9b19abbd6ec52d736695bf5eeb3055331e6
Eero af heurlin <eero.afheurlin@iki.fi>
2014-09-12 16:55:32 +0300

    made some of the method names better, added worker list method

--- gamemaster.py
+++ gamemaster.py
@@ -12,27 +12,34 @@ from config import *
 
 
 class gamemaster(zmqdecorators.client):
-    def __init__(self, mcp_wrapper):
+    def __init__(self, mcp_zmq_wrapper):
         super(gamemaster, self).__init__()
-        self.mcp_wrapper = mcp_wrapper
+        self.mcp_zmq_wrapper = mcp_zmq_wrapper
 
-    def get_proxy(self, identity):
-        return workerproxy(identity, self.mcp_wrapper)
+    def get_worker(self, identity):
+        return workerproxy(identity, self.mcp_zmq_wrapper)
+
+    def call(self, *args):
+        return zmqdecorators.call_sync(self.mcp_zmq_wrapper,  *args)
+
+    def list_workers(self):
+        # I wonder about this response format though, it's tuple that has a list as the first element..
+        return json.loads(self.call('list_workers')[0][0]).keys()
+
+
 
 class workerproxy(object):
     identity = None
-    mcp_wrapper = None
-    
-    def __init__(self, identity, mcp_wrapper):
+    mcp_zmq_wrapper = None
+
+    def __init__(self, identity, mcp_zmq_wrapper):
         self.identity = identity
-        self.mcp_wrapper = mcp_wrapper
+        self.mcp_zmq_wrapper = mcp_zmq_wrapper
 
     def cmd(self, cmd, *args):
         """The command arguments must be JSON encoded to be transferred via ZMQ"""
-        #print("Calling self.mcp_wrapper.call('send_command', %s, %s, %s)" % (self.identity, cmd, json.dumps(*args)))
-        zmqdecorators.call_sync(self.mcp_wrapper, 'send_command', self.identity, cmd, json.dumps(args))
-
-
+        #print("Calling self.mcp_zmq_wrapper.call('send_command', %s, %s, %s)" % (self.identity, cmd, json.dumps(*args)))
+        zmqdecorators.call_sync(self.mcp_zmq_wrapper, 'send_command', self.identity, cmd, json.dumps(args))
 
 if __name__ == "__main__":
     import sys,os
@@ -44,11 +51,11 @@ if __name__ == "__main__":
         MCP_SIGNALS_SERVICE = (sys.argv[1], MCP_SIGNALS_PORT)
 
     print("Connecting to MCP")
-    mcp_wrapper = zmqdecorators.zmq_bonjour_connect_wrapper(zmq.DEALER, MCP_METHODS_SERVICE)
-    print("Got MCP identity %s" % mcp_wrapper.socket.getsockopt(zmq.IDENTITY))
-    gm = gamemaster(mcp_wrapper)
-    e = gm.get_proxy('EVERYONE')
-    print("""e = gm.get_proxy('EVERYONE') # proxy for all workers created """)
-    print("""create new proxies: p = gm.get_proxy('worker_id') """)
+    mcp_zmq_wrapper = zmqdecorators.zmq_bonjour_connect_wrapper(zmq.DEALER, MCP_METHODS_SERVICE)
+    print("Got MCP identity %s" % mcp_zmq_wrapper.socket.getsockopt(zmq.IDENTITY))
+    gm = gamemaster(mcp_zmq_wrapper)
+    e = gm.get_worker('EVERYONE')
+    print("""e = gm.get_worker('EVERYONE') # proxy for all workers created """)
+    print("""create new worker proxies: p = gm.get_worker('worker_id') """)
     print("""try: e.cmd('wd:get', 'http://www.aalto.fi/') # Note this, returns as soon as MCP has queued the message to workers""")
 
