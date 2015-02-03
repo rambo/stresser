875b94b76a59e1daac962833b556e142a0060d97
Eero af heurlin <eero.afheurlin@iki.fi>
2015-02-03 12:04:20 +0200

    script to start hub and worker on current display instead of spwaning new Xvfb process

---
+++ bin/start_display.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+#java -jar /opt/selenium/server/selenium-server-standalone-current.jar -log ~/selenium.log 
+# hub
+java -jar /opt/selenium/server/selenium-server-standalone-current.jar -role hub -port 4444 -log ~/seleniumhub.log &
+HUB_PID=$!
+sleep 5
+# node
+#java -jar /opt/selenium/server/selenium-server-standalone-current.jar -role node -port 4445 -log ~/seleniumnode.log -hub http://localhost:4444/grid/register -Dwebdriver.chrome.driver=/usr/lib/chromium-browser/chromedriver
+# install real chrome via http://www.ubuntuupdates.org/ppa/google_chrome
+# Then install chromedriver (prebuilt) from http://chromedriver.storage.googleapis.com/index.html 
+java -jar /opt/selenium/server/selenium-server-standalone-current.jar -role node -port 4445 -log ~/seleniumnode.log -hub http://localhost:4444/grid/register -Dwebdriver.chrome.driver=/opt/chromedriver/chromedriver
+kill $HUB_PID
+wait $HUB_PID
